774bdc5649d8eb797ba4ad456ed1dfc54d81ce0e
Pawel <wydrapawel@icloud.com>
2022-10-17 08:21:20 +0200

    rename elements

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ screen.addshape('ship.gif')
 screen.addshape("alien.gif")
 screen.addshape("bullet.gif")
 
-main_paddle = Paddle((0, -230))
+main_ship = Paddle((0, -230))
 bullet_manager = BulletManager()
 scoreboard = Scoreboard()
 colors = ["yellow", "green", "blue", "red"]
-paddles = []
+enemies = []
 x_cor_paddles = -230
 y_cor_paddles = 30
 for y_cor in range(4):
     for _ in range(11):
         ### ADD IMG TO ENEMY ###
-        paddle = Paddle((x_cor_paddles, y_cor_paddles), "alien.gif")
-        paddle.shapesize(stretch_wid=1, stretch_len=3)
-        paddle.color(colors[y_cor])
+        enemy = Paddle((x_cor_paddles, y_cor_paddles), "alien.gif")
+        enemy.shapesize(stretch_wid=1, stretch_len=3)
+        enemy.color(colors[y_cor])
         x_cor_paddles += 45
-        paddles.append(paddle)
+        enemies.append(enemy)
     x_cor_paddles = -230
     y_cor_paddles += 60
 
 
 screen.listen()
-screen.onkey(main_paddle.go_left, "a")
-screen.onkey(main_paddle.go_right, "d")
+screen.onkey(main_ship.go_left, "a")
+screen.onkey(main_ship.go_right, "d")
 
 
 game_is_on = True
@@ -45,14 +45,13 @@ while game_is_on:
     time.sleep(0.05)
     screen.update()
     bullet_manager.move()
-    bullet_manager.create_bullet(main_paddle.position())
-    for paddle in paddles:
-        paddle.move()
+    bullet_manager.create_bullet(main_ship.position())
+    for enemy in enemies:
+        enemy.move()
         for bullet in bullet_manager.bullets:
-            if bullet.distance(paddle) < 30:
+            if bullet.distance(enemy) < 30:
                 bullet.goto(900, 900)
-                paddle.goto(800, 800)
+                enemy.goto(800, 800)
                 scoreboard.point()
 
 screen.exitonclick()
-
